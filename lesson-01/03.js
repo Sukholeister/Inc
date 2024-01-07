70e4bb91050a97a7ff263ba36917acc124a6f8ab
Andrii Sukholeister <User80794@gmail.com>
2024-01-07 19:22:21 +0200

    number 2

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,6 +1,6 @@
 let number = 1;
 
-console.log(--number);  // Вывод: 1
+console.log(number++);  // Вывод: 1
 console.log(++number);  // Вывод: 3
 console.log(number--);  // Вывод: 3
-console.log(number++);  // Вывод: 1
+console.log(--number);  // Вывод: 1
